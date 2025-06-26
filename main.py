93ec123095594965f0acbda24bce4f3951234269
dwiwad <dylanwiwad@Dylans-MacBook-Pro.local>
2025-06-26 15:14:10 -0500

    Added basic structure, routing, and HTML

--- main.py
+++ main.py
@@ -1,9 +1,84 @@
-from fastapi import FastAPI
-from app.routes import router
+# Import core FastAPI tools and classes
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+# Initialize the FastAPI app
 app = FastAPI()
-app.include_router(router)
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the site!"}
+# Mount the /static URL path to serve all the static files
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+
+# Tell FastAPI to use the "Templates" folder for the html Templates
+templates = Jinja2Templates(directory = "templates")
+
+# -----------------------------------------------------------------------
+# Route: Homepage ('/')
+# Loads templates/index.html
+# -----------------------------------------------------------------------
+
+@app.get("/", response_class = HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# -----------------------------------------------------------------------
+# Route: About Page ('/about')
+# Loads templates/about.html
+# -----------------------------------------------------------------------
+
+@app.get("/about", response_class = HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+# -----------------------------------------------------------------------
+# Route: Blog Page ('/blog')
+# Loads templates/blog/index.html
+# -----------------------------------------------------------------------
+
+@app.get("/blog", response_class = HTMLResponse)
+async def blog(request: Request):
+    return templates.TemplateResponse("blog/index.html", {"request": request})
+
+# -----------------------------------------------------------------------
+# Route: Player history Blog Page Page ('/blog/historical_player_analysis.html')
+# Loads templates/blog/historical_player_analysis.html
+# -----------------------------------------------------------------------
+
+@app.get("/blog/historical_player_analysis", response_class = HTMLResponse)
+async def blog_hist_analysis_post(request: Request):
+    return templates.TemplateResponse("blog/historical_player_analysis.html", {"request": request})
+
+# -----------------------------------------------------------------------
+# Route: Dashboard page ('/dashboard')
+# Loads templates/dashboard.html
+# -----------------------------------------------------------------------
+
+@app.get("/dashboard", response_class = HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
