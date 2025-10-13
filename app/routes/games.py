79c12f54cb6bf73edec0b1a47351ab1dcaa2e2db
Dylan Wiwad <dwiwad@gmail.com>
2025-10-13 18:56:43 -0500

    Create the depth deep dive sub-page

--- app/routes/games.py
+++ app/routes/games.py
@@ -34,8 +34,8 @@ async def game_dashboard(request: Request, season: int, game_id: int, fresh: boo
         pbp=pbp,
         roster_rows=rosters,
         xg_df=xg,
-        situation="all",        # or "5on5"
-        adjusted=False,         # True for flurry/score/venue adjusted if available
+        situation="all",        
+        adjusted=False,         
         include_goalies=False,
     )
 
@@ -52,6 +52,58 @@ async def game_dashboard(request: Request, season: int, game_id: int, fresh: boo
 
     return templates.TemplateResponse(
         "game_dashboard.html",
+        {"request": request, 
+         "game_id": game_id, 
+         "season": season,
+         "sog": data, 
+         "hits": hits,
+         "xg": xg,
+         "box": boxscore,
+         "clock": clock,
+         "rosters": rosters,
+         "shot_depth_payload": shot_depth_payload,
+         "cf_depth_payload": cf_depth_payload,
+         "xg_depth_payload": xg_depth_payload,
+         "toi_depth_payload": toi_depth_payload,
+         }
+    )
+
+@games_router.get("/dashboard/games/{season}/{game_id}/depth")
+async def game_depth_breakdown(request: Request, season: int, game_id: int):
+    # Fetch same data as game_dashboard
+        # Service Functions to actually get and store data
+    pbp = fetch_game_pbp(game_id, season, ttl_seconds=5)
+    box = fetch_game_box(game_id, season, ttl_seconds=5)
+    xg = fetch_moneypuck_player_xg_csv(game_id, season, ttl_seconds=5)
+    shifts = fetch_game_shifts(game_id, season, ttl_seconds=5)
+
+    rosters = roster_by_team(pbp)
+    shot_depth_payload = shot_depth_from_pbp(pbp, rosters) 
+    cf_depth_payload = cf_depth_from_pbp(pbp, rosters)
+    toi_depth_payload = toi_depth_from_shifts(pbp, shifts, rosters)
+
+    xg_depth_payload = xgoal_depth_from_players(
+        pbp=pbp,
+        roster_rows=rosters,
+        xg_df=xg,
+        situation="all",        
+        adjusted=False,         
+        include_goalies=False,
+    )
+
+    # Parsers to actually work with the data
+    data = sog_by_team(pbp)
+    hits = hits_by_team(pbp)
+    boxscore = scoreboard(pbp)
+    clock_data = box.get("clock", {}) or {}
+    clock = {
+    "inIntermission": clock_data.get("inIntermission"),
+    "timeRemaining": clock_data.get("timeRemaining"),
+    "running": clock_data.get("running")
+    }
+
+    return templates.TemplateResponse(
+        "depth.html",
         {"request": request, 
          "game_id": game_id, 
          "season": season,
