394d9a0edd0514f7b3b7b16389231b32b1087a4b
Dylan Wiwad <dwiwad@gmail.com>
2025-11-13 15:42:21 -0600

    Backfilling depth, fixing a styling bug

--- scripts/backfill_depth_score.py
+++ scripts/backfill_depth_score.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 # else:
 #     logger.info("No existing master file")
 
-start_date = date(2025, 10, 30)
+start_date = date(2025, 11, 8)
 end_date = date.today() - timedelta(days=1)  # Yesterday
 
 total_games = 0
