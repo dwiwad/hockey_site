728be8b337bf3afedc79e97c991f19ccbecdef35
Dylan Wiwad <dwiwad@gmail.com>
2026-01-10 20:11:57 -0600

    Small link fixes

--- app/nhl/league_stats.py
+++ app/nhl/league_stats.py
@@ -182,7 +182,7 @@ def create_league_depth_boxplot(df_rolling):
           # Title
           title=dict(
               text=f"Team Depth over the last 10 games ({get_current_season()}-{str(get_current_season() + 1)[-2:]} Season)",
-              subtitle=dict(text="Ten-game rolling average of TDI; See here for methodology."),
+              subtitle=dict(text="Ten-game rolling average of TDI; See <a href='/deep-dives/tdi-explainer' target='_blank' rel='noopener noreferrer' style='color: #E76F2B;'>here</a> for methodology."),
               font=dict(
                   family="Charter, Bitstream Charter, Sitka Text, Cambria, serif",
                   size=24,
@@ -544,7 +544,7 @@ def create_team_quadrant_scatter(team_data):
       fig.update_layout(
           title=dict(
               text=f"Team Identity: Depth vs Goaltending ({get_current_season()}-{str(get_current_season() + 1)[-2:]} Season)",
-              subtitle=dict(text="Depth and Goalie performance relative to opponents; See here for methodology."),
+              subtitle=dict(text="Depth and Goalie performance relative to opponents; See <a href='/deep-dives/depth-and-goaltending' target='_blank' rel='noopener noreferrer style='color: #E76F2B;'>here</a> for methodology."),
               font=dict(
                   family="Charter, Bitstream Charter, Sitka Text, Cambria, serif",
                   size=24,
